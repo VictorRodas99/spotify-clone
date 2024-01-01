d148bead3b55b9eb6ac88bc9f0cf50539bdd42f6
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-01-01 19:19:54 -0300

    add new color + fix deprecated colors warning

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,26 @@
+import colors from 'tailwindcss/colors'
+
+/* As of Tailwind CSS v2.2, certain colors has been renamed  */
+const deprecatedColors = [
+	'lightBlue',
+	'warmGray',
+	'coolGray',
+	'blueGray',
+	'trueGray'
+]
+
+deprecatedColors.forEach((color) => delete colors[color])
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		colors: {
+			'primary-dark': '#121212',
+			...colors // to get the default tailwind colors as well
+		}
 	},
 	plugins: [],
 }
