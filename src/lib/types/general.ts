632556a453f20d3dc43c37bd5d540402dcf2accf
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-01-14 19:33:59 -0300

    add album and artist interfaces

--- src/lib/types/general.ts
+++ src/lib/types/general.ts
@@ -1,3 +1,12 @@
-export interface ProfileContent {
-  type: 'playlist' | 'album' | 'artist'
+export interface Album {
+  name: string
+  artists: string[]
+  type: 'album'
+  // the songs are going to be fetched, not hardcoded
+  // the data (date release, time, cover, etc) are going to be fetched as well
+}
+
+export interface Artist {
+  name: string
+  type: 'artist'
 }
