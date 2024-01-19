41e4c4d9037fb50380f7ebe08474035275f1317d
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-01-19 10:59:52 -0300

    create suggestions data based on profile

---
+++ src/lib/suggestions.ts
@@ -0,0 +1,57 @@
+import type { Playlist } from './types/playlist'
+import type { Album, Artist } from './types/general'
+import { playlists } from './playlists'
+import { content } from './types/general'
+
+const [firstPlaylist, secondPlaylist] = playlists
+
+export const greetingsSection: (Playlist | Album | Artist)[] = [
+  {
+    name: 'Dream, Ivory',
+    artists: ['Dream, Ivory'],
+    type: content.album
+  },
+  {
+    name: 'Pablo Honey',
+    artists: ['Radiohead'],
+    type: content.album
+  },
+  {
+    ...firstPlaylist
+  },
+  {
+    name: 'Mazzy Star',
+    type: content.artist
+  },
+  {
+    name: 'Starflyer 59',
+    artists: ['Starflyer 59'],
+    type: content.album
+  },
+  {
+    ...secondPlaylist
+  }
+]
+
+export const secondSection: Album[] = [
+  {
+    name: 'loveless',
+    artists: ['my bloody valentine'],
+    type: content.album
+  },
+  {
+    name: 'm b v',
+    artists: ['my bloody valentine'],
+    type: content.album
+  },
+  {
+    name: 'The Bends',
+    artists: ['Rediohead'],
+    type: content.album
+  },
+  {
+    name: 'Delaware',
+    artists: ['Drop Nineteens'],
+    type: content.album
+  }
+]
