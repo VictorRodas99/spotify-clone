f0eec6fbe38c6cf897ffb6f8f0253333c18e2580
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-01-20 21:02:11 -0300

    add new color context to grab css variables

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,21 +11,22 @@ const deprecatedColors = [
 
 deprecatedColors.forEach((color) => delete colors[color])
 
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 		colors: {
+			context: 'var(--context-color)',
 			'primary-dark': {
 				50: '#1a1a1a',
-				100: '#121212'
+				100: '#121212',
+				200: '#181818'
 			},
-			'light': '#b3b3b3',
+			light: '#b3b3b3',
 			'spotify-green': '#1ed760',
 			...colors // to get the default tailwind colors as well
 		}
 	},
-	plugins: [],
+	plugins: []
 }
