8a58902866eaba91b0f85d7db1268ebcbe44c0a7
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-01-27 19:34:30 -0300

    add new utility to format time

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -3,3 +3,25 @@ export function getLocalHour() {
     new Date().toLocaleTimeString([], { hour12: false, hour: '2-digit' })
   )
 }
+
+export function formatSeconds(
+  seconds: number,
+  options?: { stringify?: boolean }
+) {
+  if (typeof seconds !== 'number' || isNaN(seconds) || seconds < 0) {
+    console.error(
+      'Invalid input, expected a non-negative number representing seconds'
+    )
+    return options?.stringify ? '0 min 0 s' : '0:00'
+  }
+
+  const minutes = Math.floor(seconds / 60)
+  const remainingSeconds = seconds % 60
+
+  const parsedMinutes = String(minutes).padStart(2, '0')
+  const parsedSeconds = String(remainingSeconds).padStart(2, '0')
+
+  return options?.stringify
+    ? `${parsedMinutes} min ${parsedSeconds} s`
+    : `${parsedMinutes}:${parsedSeconds}`
+}
