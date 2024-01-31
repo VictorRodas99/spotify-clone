4b593588841d8b80f8f1159b142cb9a20ccfdbb2
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-01-31 11:20:33 -0300

    Revert "add new utility to format image main colors as an object of hex strings"
    This reverts commit 5236ece3baf804d8413dfe30e98e4c9da8c2d94d.

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,5 +1,3 @@
-import Vibrant from 'node-vibrant'
-
 export function getLocalHour() {
   return Number(
     new Date().toLocaleTimeString([], { hour12: false, hour: '2-digit' })
@@ -26,45 +24,4 @@ export function formatSeconds(
   return options?.stringify
     ? `${parsedMinutes} min ${parsedSeconds} s`
     : `${parsedMinutes}:${parsedSeconds}`
-}
-
-interface Palette {
-  primaryDarkColor: string
-  secondaryDarkColor: string
-  mutedColor: string
-  primaryLightColor: string
-  lightMutedColor: string
-}
-/**
- * Returns values as hex strings
- */
-export async function getPaletteFromImage({
-  source,
-  colorFallback = '#000'
-}: {
-  source: string
-  colorFallback?: string
-}): Promise<Palette> {
-  if (typeof source !== 'string') {
-    console.error('Expected image source to be a string')
-
-    return {
-      primaryDarkColor: colorFallback,
-      secondaryDarkColor: colorFallback,
-      mutedColor: colorFallback,
-      primaryLightColor: colorFallback,
-      lightMutedColor: colorFallback
-    }
-  }
-
-  const vibrant = new Vibrant(source)
-  const palette = await vibrant.getPalette()
-
-  return {
-    primaryDarkColor: palette.Vibrant?.hex ?? colorFallback,
-    secondaryDarkColor: palette.DarkVibrant?.hex ?? colorFallback,
-    mutedColor: palette.Muted?.hex ?? colorFallback,
-    primaryLightColor: palette.LightVibrant?.hex ?? colorFallback,
-    lightMutedColor: palette.LightMuted?.hex ?? colorFallback
-  }
 }
