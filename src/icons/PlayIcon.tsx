c1e5c7386b84f613b19189ed32572d7f6ab1d52d
VictorRodas99 <victormatiasrodas100@gmail.com>
2024-03-16 18:08:52 -0300

    rename icons

--- src/icons/PlayIcon.tsx
+++ src/icons/PlayIcon.tsx
@@ -1,17 +1,16 @@
----
-interface Props {
-  className?: string
-  size?: number
+import type IconProps from './types/icon-generic-type'
+
+export default function PlayIcon({ className, size, ...props }: IconProps) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={size ?? 28}
+      height={size ?? 28}
+      fill="currentColor"
+      className={className}
+      {...props}
+    >
+      <path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path>
+    </svg>
+  )
 }
-
-const { className, size } = Astro.props
----
-
-<svg
-  viewBox="0 0 24 24"
-  width={size ?? 28}
-  height={size ?? 28}
-  fill="currentColor"
-  class={className}
-  ><path fill="currentColor" d="M8 5.14v14l11-7-11-7z"></path></svg
->
---
+++ src/icons/types/icon-generic-type.d.ts
@@ -0,0 +1,4 @@
+export default interface IconProps extends React.HTMLProps<SVGSVGElement> {
+  className?: string
+  size?: number
+}
